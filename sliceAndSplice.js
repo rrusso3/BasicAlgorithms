75b448a911dfe8c899e27714925b432db58f2de3
rrusso3 <rrusso3@binghamton.edu>
2018-06-06 20:19:38 -0400

    One algorithm change to match problem statement

--- sliceAndSplice.js
+++ sliceAndSplice.js
@@ -1,11 +1,14 @@
 function frankenSplice(arr1, arr2, n) {
-  var segA = arr2.slice(0, n);
-  var segB = arr2.slice(n);    //only works for arrays of primitives
-  var segM = arr1;
+  var newArr1 = arr1.slice();
+  var newArr2 = arr2.slice();
 
-  console.log(segB);
+  for (var i=0; i<newArr1.length; i++){
+    newArr2.splice(n, 0, newArr1[i]);
+    n++
+  }
 
-  return segA.concat(segM, segB);
+  return newArr2;
 }
+
 
 frankenSplice([1, 2, 3], [4, 5, 6], 1);
